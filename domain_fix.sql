8e6121be40dc3a2528ced640cc0f1c3e78562fe1
Gábor Angyal <gabor.angyal@codesharp.hu>
2023-04-20 09:38:35 +0200

    Update domain_fix.sql

--- domain_fix.sql
+++ domain_fix.sql
@@ -3,3 +3,5 @@ select id, post_title, guid from wp_posts where post_content like '%www.oldsiteu
 
 UPDATE wp_posts SET post_content = REPLACE (post_content, 'https://www.oldsiteurl.com', 'https://www.newsiteurl.com');
 UPDATE wp_posts SET guid = REPLACE (guid, 'https://www.oldsiteurl.com', 'https://www.newsiteurl.com');
+UPDATE wp_postmeta SET meta_value = REPLACE (meta_value, 'https://www.oldsiteurl.com', 'https://www.newsiteurl.com');
+
