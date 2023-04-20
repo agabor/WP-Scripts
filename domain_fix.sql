c95c20b8269b8e50deb63c6efabb8b84e67da8c9
Gábor Angyal <gabor.angyal@codesharp.hu>
2023-04-20 08:56:45 +0200

    Update domain_fix.sql

--- domain_fix.sql
+++ domain_fix.sql
@@ -1,2 +1,5 @@
+select id, post_title, guid from wp_posts where guid like '%www.oldsiteurl.com%'
+select id, post_title, guid from wp_posts where post_content like '%www.oldsiteurl.com%'
+
 UPDATE wp_posts SET post_content = REPLACE (post_content, 'https://www.oldsiteurl.com', 'https://www.newsiteurl.com');
 UPDATE wp_posts SET guid = REPLACE (guid, 'https://www.oldsiteurl.com', 'https://www.newsiteurl.com');
