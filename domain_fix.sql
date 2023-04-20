588373c370bdf0ca4f13b688ce038f956f197a97
Gábor Angyal <gabor.angyal@codesharp.hu>
2023-04-20 09:42:49 +0200

    Update domain_fix.sql

--- domain_fix.sql
+++ domain_fix.sql
@@ -1,7 +1,6 @@
-select id, post_title, guid from wp_posts where guid like '%www.oldsiteurl.com%'
-select id, post_title, guid from wp_posts where post_content like '%www.oldsiteurl.com%'
+set @olddomain = 'www.oldsiteurl.com';
+set @newdomain = 'www.newsiteurl.com';
 
-UPDATE wp_posts SET post_content = REPLACE (post_content, 'https://www.oldsiteurl.com', 'https://www.newsiteurl.com');
-UPDATE wp_posts SET guid = REPLACE (guid, 'https://www.oldsiteurl.com', 'https://www.newsiteurl.com');
-UPDATE wp_postmeta SET meta_value = REPLACE (meta_value, 'https://www.oldsiteurl.com', 'https://www.newsiteurl.com');
-
+UPDATE wp_posts SET post_content = REPLACE (post_content, @olddomain, @newdomain);
+UPDATE wp_posts SET guid = REPLACE (guid, @olddomain, @newdomain);
+UPDATE wp_postmeta SET meta_value = REPLACE (meta_value, @olddomain, @newdomain);
