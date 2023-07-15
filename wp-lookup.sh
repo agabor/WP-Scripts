45f08a2594ab09642d69dc24ef7397a656d1da88
Gábor Angyal <gabor.angyal@codesharp.hu>
2023-07-15 18:06:32 +0200

    Update wp-lookup.sh

--- wp-lookup.sh
+++ wp-lookup.sh
@@ -11,7 +11,8 @@ grep -rnw "$1/wp-content/plugins" -e "$2"
 db_name=$(grep DB_NAME $1/wp-config.php | awk -F "'" '{print $4}')
 db_user=$(grep DB_USER $1/wp-config.php | awk -F "'" '{print $4}')
 db_pass=$(grep DB_PASSWORD $1/wp-config.php | awk -F "'" '{print $4}')
+table_prefix=$(grep table_prefix $1/wp-config.php | awk -F "'" '{print $2}')
 
 echo -e "\n\nsearching snipets\n\n"
 
-echo "select id, name, description, code from wp_fitness_snippets where code like \"%$2%\";" | mariadb --host="localhost" --user="$db_user" --database="$db_name" --password="$db_pass"
+echo "select id, name, description, code from ${table_prefix}snippets where code like \"%$2%\";" | mariadb --host="localhost" --user="$db_user" --database="$db_name" --password="$db_pass"
