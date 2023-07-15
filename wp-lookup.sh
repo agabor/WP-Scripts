da287b70f4b9c46b89c6ec17053b363cdd4316f6
Gábor Angyal <gabor.angyal@codesharp.hu>
2023-07-15 17:54:17 +0200

    Create wp-lookup.sh

---
+++ wp-lookup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+  
+echo -e "searching wp-includes\n\n"
+
+grep -rnw "$1/wp-includes/" -e "$2"
+
+echo -e "\n\nsearching plugins\n\n"
+
+grep -rnw "$1/wp-content/plugins" -e "$2"
+
+db_name=$(grep DB_NAME $1/wp-config.php | awk -F "'" '{print $4}')
+db_user=$(grep DB_USER $1/wp-config.php | awk -F "'" '{print $4}')
+db_pass=$(grep DB_PASSWORD $1/wp-config.php | awk -F "'" '{print $4}')
+
+echo -e "\n\nsearching snipets\n\n"
+
+echo "select id, name, description, code from wp_fitness_snippets where code like \"%$2%\";" | mariadb --host="localhost" 
+--user="$db_user" --database="$db_name" --password="$db_pass"
